9d051e82f125e0adfa7365a78d6bff3fb86a67d4
Waseem Ahmed <codewaseem@gmail.com>
2020-03-14 13:04:23 +0530

    using styled-components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,32 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
+import styled from "styled-components"
+
+const Container = styled.div`
+  margin: 3rem auto;
+  max-width: 600px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+const Avatar = styled.img`
+  flex: 0 0 96px;
+  width: 96px;
+  height: 96px;
+  margin: 0;
+`
+const Username = styled.h2`
+  margin: 0 0 12px 0;
+  padding: 0;
+`
+const User = props => (
+  <>
+    <Avatar src={props.avatar} alt={props.username} />
+    <Username>{props.username}</Username>
+  </>
+)
 
 const IndexPage = () => (
   <Layout>
@@ -10,6 +35,18 @@ const IndexPage = () => (
       <Link to="/auto">Another page auto generated</Link>
       <Link to="/section/about">Nested page link</Link>
     </section>
+    <Container>
+      <h1>About Styled Components</h1>
+      <p>Styled Components is cool</p>
+      <User
+        username="Waseem Ahmed"
+        avatar="https://media-exp1.licdn.com/dms/image/C5103AQHgq6PMyzXXKA/profile-displayphoto-shrink_200_200/0?e=1589414400&v=beta&t=A9bU-Ta2ZzyHggbuP6NNkUIBRNiJqAA7C8cNgMt3Y1E"
+      />
+      <User
+        username="Waseem Ahmed"
+        avatar="https://media-exp1.licdn.com/dms/image/C5103AQHgq6PMyzXXKA/profile-displayphoto-shrink_200_200/0?e=1589414400&v=beta&t=A9bU-Ta2ZzyHggbuP6NNkUIBRNiJqAA7C8cNgMt3Y1E"
+      />
+    </Container>
   </Layout>
 )
 
