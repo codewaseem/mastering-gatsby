602da7c6a55b18e0014dd899f301914659a4151a
Waseem Ahmed <codewaseem@gmail.com>
2020-03-14 14:08:34 +0530

    use scss

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
