498292bfd16f4fb9a34c5e25e416d2582a8d88d7
Waseem Ahmed <codewaseem@gmail.com>
2020-03-14 12:31:55 +0530

    auto pages and links

---
+++ src/pages/auto.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const AutoPage = () => {
+  return (
+    <section>
+      <h1>Automatically Creating Pages</h1>
+      <p>
+        React components which are exported as default inside the /pages
+        directory are automatically created as pages with their folder/file name
+        structure
+      </p>
+    </section>
+  )
+}
+
+export default AutoPage
---
+++ src/pages/section/about.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const AboutSection = () => {
+  return <div>The path of this page should be /section/about</div>
+}
+
+export default AboutSection
