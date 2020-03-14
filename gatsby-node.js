58288d0089182cba4541dc0cb7bd0fbac5b2046d
Waseem Ahmed <codewaseem@gmail.com>
2020-03-14 11:52:06 +0530

    create pages programmatically

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,45 @@
  */
 
 // You can delete this file if you're not using it
+
+const path = require("path")
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+
+  const result = await graphql(
+    `
+      {
+        allSitePlugin {
+          nodes {
+            name
+          }
+          totalCount
+        }
+      }
+    `
+  )
+
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
+
+  const pluginsInfoTemplate = path.resolve(
+    __dirname,
+    `src/templates/pluginsInfo.js`
+  )
+  const pluginNames = Array.from(
+    new Set(result.data.allSitePlugin.nodes.map(node => node.name))
+  )
+  createPage({
+    path: "/pluginsInfo",
+    component: pluginsInfoTemplate,
+    context: {
+      data: {
+        names: pluginNames,
+        totalCount: pluginNames.length,
+      },
+    },
+  })
+}
