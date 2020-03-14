57a346f00aecacb69d866655f2110ac75aed0b5f
Waseem Ahmed <codewaseem@gmail.com>
2020-03-14 14:59:49 +0530

    use google fonts locally using typeface

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -2,12 +2,19 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
+import "typeface-allerta"
 
 const SecondPage = () => (
   <Layout>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <section
+      style={{
+        fontFamily: "Allerta",
+      }}
+    >
+      <h1>Hi from the second page, I have a different font-family</h1>
+      <p>Welcome to page 2</p>
+      <Link to="/">Go back to the homepage</Link>
+    </section>
   </Layout>
 )
 
