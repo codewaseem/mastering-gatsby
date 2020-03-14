923cf00cec3fc5bfeb0e1ded3b9f35dcd8b6abb6
Waseem Ahmed <codewaseem@gmail.com>
2020-03-14 16:14:38 +0530

    source own data to graphql layer

--- gatsby-node.js
+++ gatsby-node.js
@@ -62,3 +62,22 @@ exports.onCreatePage = ({ page, actions }) => {
     createPage(page)
   }
 }
+
+exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
+  const pokemons = [
+    { name: "Pikachu", type: "electric" },
+    { name: "Squirtle", type: "water" },
+  ]
+  pokemons.forEach(pokemon => {
+    const node = {
+      name: pokemon.name,
+      type: pokemon.type,
+      id: createNodeId(`Pokemon-${pokemon.name}`),
+      internal: {
+        type: "Pokemon",
+        contentDigest: createContentDigest(pokemon),
+      },
+    }
+    actions.createNode(node)
+  })
+}
